7bea5702a78c66279313d3d53002ed472678da57
tsiry <tsirymanankasina@gmail.com>
2018-09-03 00:46:33 +0400

    api start

--- src/main/java/com/tsiry/demo/Demo1Application.java
+++ src/main/java/com/tsiry/demo/Demo1Application.java
@@ -21,25 +21,5 @@ public class Demo1Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Demo1Application.class, args);
-
-		// define the job and tie it to our MyJob class
-		JobDetail job = newJob(HelloJob.class).withIdentity("HelloJob", "group1").build();
-
-		// Trigger the job to run now, and then repeat every 40 seconds
-		Trigger trigger = newTrigger().withIdentity("trigger1", "group1").startNow()
-				.withSchedule(simpleSchedule().withIntervalInSeconds(2).repeatForever()).build();
-
-		// Grab the Scheduler instance from the Factory
-		Scheduler scheduler;
-		try {
-			scheduler = StdSchedulerFactory.getDefaultScheduler();
-			// and start it off
-			scheduler.start();
-			// Tell quartz to schedule the job using our trigger
-			scheduler.scheduleJob(job, trigger);
-		} catch (SchedulerException e) {
-			e.printStackTrace();
-		}
-
 	}
 }
